072be1f462ea6234ab6618433465fb6c9148621d
GerrardE <ezeugwagerrard@gmail.com>
2018-09-15 07:15:06 +0100

    full game functionalities minus star rating implemented

--- js/app.js
+++ js/app.js
@@ -1,38 +1,150 @@
-/*
- * Create a list that holds all of your cards
- */
-var cards = document.getElementsByClassName(card);
+	//First, I got the deck,
+	const deck = document.getElementById("deck");
+	//Then I stored all the required images in my cards,
+	const cards = ["fa-diamond","fa-paper-plane-o","fa-anchor","fa-bolt","fa-cube","fa-anchor","fa-leaf","fa-bicycle","fa-diamond","fa-bomb","fa-leaf","fa-bomb","fa-bolt","fa-bicycle","fa-paper-plane-o", "fa-cube"];
+	//Here, i collect the move ID and track it on call function recordMoves,
+	let record = document.getElementById('moves');
+	let moves = 0;
+	var recordMoves =	function (){
+			moves++;    
+			record.innerHTML = moves;
+		}
+	
+	//Here, I set up the star element and add it when the recordMatch function is called
+	let stars = document.getElementById('stars');
+	let matches = document.getElementById('matched');
+	let match = 0;
+	var recordMatch =	function (){
+			match++; 
+				let image = document.createElement('i');
+				image.classList.add('fa', 'fa-star');
+				let starr = document.createElement('li');
+				starr.appendChild(image);	
+			if ((moves < 9) && (match <= 2)){
+				stars.appendChild(starr);
+				console.log(starr);
+			} else if (moves < 17 && (match > 2 || match <= 4)){
+				stars.appendChild(starr);
+			} else if (moves > 24 && (match > 4 || match <= 6)){
+				stars.appendChild(starr);
+			}
+		
+				//This If statement kicks when the matches are complete. Congratulations!
+				if (match === 8) {
+				let modal = document.getElementById('modal');
+				modal.style.display = 'block';
+				let close = document.getElementById('close');
+				close.onclick = function(){
+					modal.style.display = 'none';
+				}
+			}
+		}
+		
+		//This is the shuffle function recommemnded by Udacity, I call it to shuffle the images on the cards
+		function shuffle () {
+				var currentIndex = cards.length, temporaryValue, randomIndex;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+				while (currentIndex !== 0) {
+						randomIndex = Math.floor(Math.random() * currentIndex);
+						currentIndex -= 1;
+						temporaryValue = cards[currentIndex];
+						cards[currentIndex] = cards[randomIndex];
+						cards[randomIndex] = temporaryValue;
+				}
+				return cards;
+	} shuffle(cards);
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+	//Here, I call each image in the card, create an li, create an i element, append the image to the 'i' //and then the 'li'
+	for(const card of cards){
+			const li = document.createElement('li');
+			li.setAttribute('class', 'card');
+			deck.appendChild(li);
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+			const i = document.createElement('i');
+			i.classList.add('fa', card);
+		
+			li.appendChild(i);
+	}
+	
+			//Here, I collect all the cards in a box,
+			let boxes = document.getElementsByClassName('card');
+			//Here, I collect all matched cards in a box too,
+			let boxMatches = document.getElementsByClassName('match');		
+		
+			//This for-loop loops through the boxes and adds classes/removes classes on them
+			for (const box of boxes){
+				 var displayCard = function () {
+					box.classList.add('open');
+					box.classList.toggle('show');
+					box.classList.toggle('disabled');
+				}
+	
+				box.addEventListener('click', displayCard);
+				box.addEventListener('click', openBox);
+				var openBoxes = [];
+			
+					//This function pushes open cards to the openBox variable declared above,
+					function openBox() {
+						openBoxes.push(box);
+							if(openBoxes.length === 2){
+								recordMoves();
+								//checks that the inner html of the box match or not then call respective functions,
+									if(openBoxes[1].innerHTML === openBoxes[0].innerHTML){
+													matched(); 
+												} else {
+													unmatched();
+									}
+							};
+						
+						//This function is called from the openBox function
+						function matched() {
+							openBoxes[0].classList.add("match");
+							openBoxes[1].classList.add("match");
+							openBoxes[0].classList.remove("show", "open");
+							openBoxes[1].classList.remove("show", "open");
+							openBoxes = [];
+							recordMatch();
+						}
+						
+						//This function is called from the openBox function
+						function unmatched() {
+							openBoxes[0].classList.add("unmatched");
+							openBoxes[1].classList.add("unmatched");
+							disable();
+							
+							setTimeout(function(){
+								openBoxes[0].classList.remove("show", "open", "unmatched");
+								openBoxes[1].classList.remove("show", "open", "unmatched");
+								enable();
+								openBoxes = [];
+							},800);
+						}
+						
+						//I learned this function from Sandra at https://sandraisrael.github.io/Memory-Game-fend/#
+						//Essentially saw that call here gives us a way to "borrow" a method from one object to use //for another.
+						function disable(){
+								Array.prototype.filter.call(boxes, function(box){
+										//the disabled class makes the pointer disabled for a while
+										box.classList.add('disabled');
+								});
+						}
+					
+						function enable(){
+								Array.prototype.filter.call(boxes, function(box){
+										box.classList.remove('disabled');
+										//loop through the mathed boxes and add disabled class to them
+										for(const boxMatch of boxMatches){
+												boxMatch.classList.add('disabled');
+										}
+								});
+						}
+					};				
+		};// End of this for-loop loops through the boxes and adds classes/removes classes on them
 
-    return array;
-}
+		//
+		
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+		//Onclick restart button, this function ensures the game is restarted
+		function newGame(){
+				window.location.reload();
+		}
